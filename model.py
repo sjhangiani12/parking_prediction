574f17487ecf23eb15874af2100cfd30ba6a5762
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2019-09-18 15:56:21 +0200

    moved everything to model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import random
 import time
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM, CuDNNLSTM, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 
 SEQ_LENGTH = 12
@@ -98,3 +98,55 @@ print(f"Decreases: {train_y.count(0)}, increases: {train_y.count(1)}")
 print(
     f"Validation decreases: {validation_y.count(0)}, increases: {validation_y.count(1)}"
 )
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(
+    train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, return_sequences=True))
+model.add(Dropout(0.1))
+model.add(BatchNormalization())
+
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(2, activation='softmax'))
+
+
+opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+
+# Compile model
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+# unique file name that will include the epoch and the validation acc for that epoch
+filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}"
+checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc',
+                                                      verbose=1, save_best_only=True, mode='max'))  # saves only the best ones
+
+# Train model
+history = model.fit(
+    train_x, train_y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(validation_x, validation_y),
+    callbacks=[tensorboard, checkpoint],
+)
+
+# Score model
+score = model.evaluate(validation_x, validation_y, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# Save model
+model.save("models/{}".format(NAME))
